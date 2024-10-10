3754361564653cabede8cefd9f0d96bf02f5b7bd
NachoConort <nachoconort@gmail.com>
2024-10-10 13:03:10 -0300

    Ejercicio 3

---
+++ ejercicios/ejer3.py
@@ -0,0 +1,7 @@
+# CALCULAR LA CIRCUNFERENCIA DE UN CIRCULO
+import math
+
+radio = float(input("Ingresa el radio: "))
+circunferencia = 2 * math.pi * radio
+
+print(f"La circunferencia es: {round(circunferencia, 2)}")
