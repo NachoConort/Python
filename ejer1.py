c178e61cc10149fc61aeeed524e6adedfb93cbf9
NachoConort <nachoconort@gmail.com>
2024-10-10 13:09:41 -0300

    Operadores, Aritmetica

--- ejer1.py
+++
@@ -1,8 +0,0 @@
-# CALCULAR AREA DE RECTANGULO
-
-alto = float(input("Ingresa el alto: "))
-ancho = float(input("Ingresa el ancho: "))
-
-area = alto * ancho
-
-print(f"El area es {area}")
--- ejer2.py
+++
@@ -1,7 +0,0 @@
-# HACER CARRITO DE COMPRAS CON ITEM - PRECIO - CANTIDAD
-
-item = input("Que quieres comprar?: ")
-precio = float(input("Cual es el precio?: "))
-cantidad = int(input("Cuantos queres llevar?: "))
-total = precio * cantidad
-print(f"El precio es: ${total}")
